13376b7568d724751fa23ac12c200bc5d3ef7ec6
Andor Goetzendorff <goetzend@cs.tum.edu>
2012-09-13 12:57:51 +0200

    moved src to public_src

--- compile.sh
+++ compile.sh
@@ -2,19 +2,19 @@
 
 while [ 1 ]; do
 	RES_JADE=$(
-		jade -P -O app src/*.jade >/dev/null;
-		jade -P -O app/partials src/partials/*.jade >/dev/null;
+		jade -P -O public public_src/*.jade >/dev/null;
+		jade -P -O public/partials public_src/partials/*.jade >/dev/null;
 	)
 	RES_COFFEE=$(
-		coffee -cb -o app/js src/coffee/*.coffee 2>&1
+		coffee -cb -o public/js public_src/coffee/*.coffee 2>&1
 	)
 	RES_LESS=$(
-		lessc src/less/app.less > app/css/app.css
+		lessc public_src/less/app.less > public/css/app.css
 	)
 	[ -n "$RES_JADE" ] && notify-send "jade error" && echo "$RES_JADE"
 	[ -n "$RES_COFFEE" ] && notify-send "coffee error" && echo "$RES_COFFEE"
 	echo $RES
-	inotifywait -r -e modify -q src
+	inotifywait -r -e modify -q public_src
 	echo -e "----\n\n"
 	done
 
